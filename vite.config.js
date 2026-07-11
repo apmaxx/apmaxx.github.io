7ce34fd380d1a4e2880909075bd77e393f7eabe7
Amir Pettigew <amirpacpett@gmail.com>
2026-07-11 00:45:15 -0400

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/apmaxx.io/',
+  base: '/apmaxx.github.io/',
   plugins: [
     vue(),
     vueDevTools(),
